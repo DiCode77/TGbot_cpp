750b98f1e1ba95ce396cf69502b3555920270cee
DiCode77 <dimazamshaq@icloud.com>
2025-09-27 20:56:14 +0300

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,33 @@
-cd TGbot_cpp;
+#!/bin/bash
 
-cd Resources;
-curl -L https://github.com/nlohmann/json/releases/latest/download/json.hpp -o json.hpp;
-cd ..;
+TBOT_REPO_URL="https://github.com/DiCode77/TGbot_cpp.git"
+JSON_REPO_URL = "https://github.com/nlohmann/json/releases/latest/download/json.hpp"
 
-mkdir build;
-cd build;
-cmake .. -G "Xcode";
+DIR_NAME_REPO="TGbot_cpp"
+DIR_NAME_PRJ="TGbotCpp"
+DIR_NAME_RES="Resources"
+DIR_NAME_BUILD="Build"
+
+if [[ -d "$DIR_NAME_REPO" ]]; then
+   cd "$DIR_NAME_REPO"
+else
+    if [[ ! -d "$DIR_NAME_PRJ" ]]; then
+        git clone "$TBOT_REPO_URL"
+        cd "$DIR_NAME_REPO"
+    fi
+fi
+
+if [ ! -d "$DIR_NAME_RES" ]; then
+   mkdir "$DIR_NAME_RES"
+   cd Resources
+   curl -L "$JSON_REPO_URL" -o json.hpp
+   cd ..
+fi
+
+if [[ -d "$DIR_NAME_BUILD" ]]; then
+    rm -rf "$DIR_NAME_BUILD"
+fi
+
+mkdir -p "$DIR_NAME_BUILD"
+cd "$DIR_NAME_BUILD"
+cmake .. -G "Xcode"
